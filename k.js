d2fc5504d2f63be258e5b09efe1af6ab991ebedb
shubham <shubham14apr@gmail.com>
2022-08-08 03:57:23 -0700

    first commit

--- k.js
+++ k.js
@@ -1,10 +1,43 @@
-const object = {
-    name:'shubham',
-    age:24
-}
-let myobc = JSON.stringify(object);
-localStorage.setItem('myobj',myobc);
-console.log(localStorage)
+function savedata(event){
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const pwd =   document.getElementById('pwd').value;
 
-let myobc2 = JSON.parse(localStorage.getItem('myobj'));
-console.log(myobc2);
+    const object = {
+        email,
+        pwd
+    }
+    localStorage.setItem(object.email,JSON.stringify(object));
+    localStorage.getItem('shubham14apr@gmail.com');
+    showNAmeandPass(object); 
+
+}
+function showNAmeandPass(user){
+    
+    const parentnode = document.getElementById('list of user');
+    const childnode = `<li id=${user.email}> ${user.email} -- ${user.pwd} -- 
+    <button onclick=deletecredentials('${user.email}')> Delete user
+    <button onclick=edituser('${user.email}','${user.pwd}')> edit user </li>`
+    parentnode.innerHTML = parentnode.innerHTML + childnode
+
+}
+function deletecredentials(email){
+    console.log(email);
+    localStorage.removeItem(email)
+    removeUserFromScreen(email)
+}
+function removeUserFromScreen(email){
+    const parentnode = document.getElementById('list of user');
+    const childToDelete = document.getElementById(email)
+    parentnode.removeChild(childToDelete);
+}
+function edituser(email,pwd){
+    console.log(email);
+    document.getElementById('email').value = email
+    document.getElementById('pwd').value = pwd
+
+    deletecredentials(email)
+
+
+
+}
